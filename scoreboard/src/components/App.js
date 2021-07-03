dd6b7da9d917d57d546c530b5ef0ea723aa4a946
Jelena Feliciano <jfel217@gmail.com>
2021-07-03 16:42:43 -0400

    update

--- scoreboard/src/components/App.js
+++ scoreboard/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import { Provider } from './Context';
 import PlayerList from './PlayerList';
 import AddPlayerForm from './AddPlayerForm';
 
