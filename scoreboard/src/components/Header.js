87beaaacaacf125b083377640067acb2dbab875c
Jelena Feliciano <jfel217@gmail.com>
2021-07-05 15:06:53 -0400

    refactored consumer

--- scoreboard/src/components/Header.js
+++ scoreboard/src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
@@ -13,8 +12,6 @@ const Header = () => {
   );
 }
 
-Header.propTypes = {
-  players: PropTypes.arrayOf(PropTypes.object).isRequired
-};
+
 
 export default Header;
